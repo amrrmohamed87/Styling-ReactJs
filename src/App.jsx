8d9fbf0c198ed44a5b4547f0d647d20db2b4ebd6
Amr <frank1276smith@gmail.com>
2023-12-10 19:38:24 +0200

    change fragment to <></>

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Header from "./components/Header/Header.jsx";
 
 export default function App() {
   return (
-    <Fragment>
+    <>
       <Header />
       <main>
         <AuthInputs />
       </main>
-    </Fragment>
+    </>
   );
 }
